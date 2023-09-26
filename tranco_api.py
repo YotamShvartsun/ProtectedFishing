589c849d8b818d096cff0d54a8b8435ad8cea236
Yotam Shvartsun <yotam.shvartsun@gmail.com>
2023-09-26 15:36:47 +0300

    ~ Added the tranco cache to gitignore, some cr changes

--- tranco_api.py
+++ tranco_api.py
@@ -1,9 +1,6 @@
 from typing import List
 from tranco import Tranco
 
-import datetime
-
-
 class TrancoDbNotInitilizedError(Exception):
     pass
 
@@ -33,12 +30,8 @@ class TrancoApi:
         raise TrancoDbNotInitilizedError('DB is not initialized')
 
     def set_db(self) -> None:
-        new_tranco_cache_dir = self._TRANCO_BASE_CACHE_DIR + \
-            str(datetime.datetime.now())
-        new_tranco_db = Tranco(cache=True, cache_dir=new_tranco_cache_dir)
-        new_latest_list = new_tranco_db.list()
-        new_top_sites_list = new_latest_list.top()
-        self._tranco_cache_dir = new_tranco_cache_dir
-        self._tranco_db = new_tranco_db
-        self._latest_list = new_latest_list
+        if self._tranco_db is None:
+            self._tranco_db = Tranco(cache=True, cache_dir=self._TRANCO_BASE_CACHE_DIR)
+        self._latest_list = self._tranco_db.list()
+        new_top_sites_list = self._latest_list.top()
         self._top_sitest_list = new_top_sites_list
