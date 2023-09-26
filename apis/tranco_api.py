5dd0db836fd9a630f318fdda33fd8e054b5d8942
Yotam Shvartsun <yotam.shvartsun@gmail.com>
2023-09-26 22:51:29 +0300

    Not using the tranco files

--- apis/tranco_api.py
+++ apis/tranco_api.py
@@ -1,12 +1,10 @@
 from typing import List
 from tranco import Tranco
 from tranco.tranco import TrancoList
-
 from apis.base_db_api import BaseDBAPI
 
 class TrancoDbNotInitilizedError(Exception):
     pass
-
 
 # The tranco.rank function returns -1 when the domain is not in the list
 TRANCO_NOT_FOUND = -1
@@ -39,10 +37,10 @@ class TrancoApi(BaseDBAPI):
     def is_site_safe(self, domain: str) -> bool:
         if self._latest_list is None:
             self.set_db()
-        return TRANCO_NOT_FOUND != self._latest_list.rank(domain)
+        return self._latest_list.rank(domain)
 
     def set_db(self) -> None:
         if self._tranco_db is None:
-            self._tranco_db = Tranco(cache=True, cache_dir=self._TRANCO_BASE_CACHE_DIR)
+            self._tranco_db = Tranco(cache=False, cache_dir=self._TRANCO_BASE_CACHE_DIR)
         self._latest_list = self._tranco_db.list()
         self._top_sitest_list = self._latest_list.top()
