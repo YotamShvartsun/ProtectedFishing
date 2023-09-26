463e4868a7436a1debd82a6e8b6a8519ee5e9320
סיון ארזי <sivan280902@gmail.com>
2023-09-26 21:30:41 +0300

    bugfix to db bootstrapper

--- dbs_bootstrapper.py
+++ dbs_bootstrapper.py
@@ -1,19 +1,20 @@
-from typehints import Dict
-import base_db_api
-
+from typing import Dict
+from base_db_api import BaseDBAPI
+from tranco_api import TrancoApi
 
 class DBFactory:
-    def __init__():
-        self.dbNameToObject: Dict[str, BaseDbApi]  = {}
+    def __init__(self):
+        self.dbNameToObject: Dict[str, BaseDBAPI]  = {}
     
-    def add_db(dbName: str, baseDbApi: BaseDbApi):
+    def add_db(self, dbName: str, baseDbApi: BaseDBAPI):
         self.dbNameToObject[dbName] = baseDbApi
 
-    def get_db(dbName: str):
-        self.dbNameToObject[dbName]
+    def get_db(self, dbName: str) -> BaseDBAPI:
+        return self.dbNameToObject[dbName]
 
 
 
 def initialize_dbs() -> DBFactory:
     dbFactory = DBFactory()
-    """dbFactory.add_db("Tranco", TrancoApi)"""
+    dbFactory.add_db("Tranco", TrancoApi)
+    return dbFactory
