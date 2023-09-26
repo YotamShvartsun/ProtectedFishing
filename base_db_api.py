9639e5c5ee523c25ea7b197ccbd47b0648aa9c70
Yotam Shvartsun <yotam.shvartsun@gmail.com>
2023-09-26 17:57:40 +0300

    Defined abstract

--- base_db_api.py
+++ base_db_api.py
@@ -1,6 +1,6 @@
-class BaseDBAPI:
-    def __init__(self):
-        self._latest_list = None
-    
+import abc
+
+class BaseDBAPI(abc.ABC):
+    @abc.abstractmethod
     def is_site_safe(self, domain: str) -> bool:
         raise NotImplementedError() 
