0bc3f817441f20ce53c14f420d395aa4fc32ff83
סיון ארזי <sivan280902@gmail.com>
2023-09-27 15:55:05 +0300

    fix

--- apis/israelis_api.py
+++ apis/israelis_api.py
@@ -1,13 +1,17 @@
 from apis.base_db_api import BaseDBAPI
 from apis.db_type import DBType
-import csv
+from typing import List
 
 class IsraelisApi(BaseDBAPI):
     def __init__(self, dbType: DBType):
         super().__init__(dbType)
+        self._sits_list: List[str] = self._set_sits_list()
 
     async def is_in_db(self, domain: str) -> bool:
+        return domain in self._sits_list
+    
+    def _set_sits_list(self) -> List[str]:
         file = open("apis/israeli_sits.csv", "r")
         data = file.read().split('\n')
         file.close()
-        return domain in data
+        return data
