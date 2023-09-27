e73241b66533ff1174c8d20068ccffe19447d8d4
Harel Bar <harel.games@gmail.com>
2023-09-27 15:08:19 +0300

    Add minimal UI to app

--- app/app/src/main/java/com/example/protectedfishing/MainActivity.java
+++ app/app/src/main/java/com/example/protectedfishing/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.protectedfishing;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.Settings;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -26,23 +26,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void onClick(View view){
+        Intent i = new Intent();
+        i.setAction(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        i.putExtra("android.provider.extra.APP_PACKAGE", getPackageName());
+        startActivity(i);
     }
 
     @Override
