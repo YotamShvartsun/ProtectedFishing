5bc50ef6d42b57bb01f01413ffa7e76de0fb832e
סיון ארזי <sivan280902@gmail.com>
2023-09-27 10:40:30 +0300

    improve validation response

--- apis/validate_response.py
+++ apis/validate_response.py
@@ -1,5 +1,10 @@
 class ValidationResponse:
     status: str
     isInDb: bool
-    isSafe: bool
-    message: str
+    isSafe: IsSiteSafe
+    message: str = None
+
+class IsSiteSafe(IntEnum):
+    Yes = 0
+    No = 1
+    Unknown = 2
