4038f059f988e863bd2840308c5bbe7431b70a7b
Yotam Shvartsun <yotam.shvartsun@gmail.com>
2023-09-27 15:21:30 +0300

    Now using the unsafe list!

--- apis/dbs_bootstrapper.py
+++ apis/dbs_bootstrapper.py
@@ -3,6 +3,7 @@ import logging
 from typing import Dict, List
 from apis.base_db_api import BaseDBAPI
 from apis.iplocation import IPLocationDBAPI
+from apis.known_phishing_links import KnownURLSAPI
 from apis.tranco_api import TrancoApi
 from apis.db_type import DBType
 from apis.vt_scan_api import VtApi
@@ -26,6 +27,7 @@ def initialize_dbs() -> DBFactory:
     dbFactory = DBFactory()
     dbFactory.add_db("Tranco", TrancoApi(DBType.WhiteList))
     dbFactory.add_db("Geolocation", IPLocationDBAPI(DBType.BlackList))
+    dbFactory.add_db("KnownLinksDB", KnownURLSAPI(DBType.BlackList))
     # dbFactory.add_db("VirusTotal", VtApi(DBType.BlackList))
     _LOGGER.info("Done initing DBs!")
     return dbFactory
