6949857c5985cc7aa8400cca62ffdde9ec330252
סיון ארזי <sivan280902@gmail.com>
2023-09-27 10:40:01 +0300

    change base class logic from 'is safe' to 'is in db'

--- apis/base_db_api.py
+++ apis/base_db_api.py
@@ -1,6 +1,5 @@
 import abc
 from db_type import DBType
-from validate_response import ValidationResponse
 
 class BaseDBAPI(abc.ABC):
     @abc.abstractmethod
@@ -8,5 +7,5 @@ class BaseDBAPI(abc.ABC):
         self.type = type
 
     @abc.abstractmethod
-    def is_site_safe(self, domain: str) -> bool:
+    def is_in_db(self, domain: str) -> bool:
         raise NotImplementedError()
